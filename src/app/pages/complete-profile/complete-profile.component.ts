8fcb7bb8815e681605802fb3d0fc79244cbc3cf1
miaslee <iamjeremy564@gmail.com>
2024-11-16 14:51:29 -0500

    CompletarPerfil: validación de todos los campos

--- src/app/pages/complete-profile/complete-profile.component.ts
+++ src/app/pages/complete-profile/complete-profile.component.ts
@@ -33,38 +33,49 @@ let lastname =  InputApellido.value;
 let carrera =  InputCarrera.value;
 let fechaNaci = InputFN.value;
 
-const user = this.auth.getAuth().currentUser;
-if (user) {
-  this.firestore.create({
-    path: ["Users", user.uid],
-    data: {
-      publicName: name,
-      publicLastname: lastname,
-      publicCareer: carrera,
-      publicFN: fechaNaci,
-      publicSex: sexo,
-      userId: this.auth.getAuth().currentUser?.uid,
-      photoUrl: "https://devmiasx.com/uploads/67380645a9ac3_user.png"
-
-    },
-    onComplete: (docId: string) => {
-      alert("Perfil creado");
 
 
-      InputNombre.value = "";
-       InputApellido.value ="";
-       InputCarrera.value="";
-       InputFN.value="";
-
-    },
-    onFail: (err: any) => {
-      alert(err);
-    }
-  });
-} else {
-  alert("No hay un usuario autenticado.");
+if(this.isNotEmpty(name)&& this.isNotEmpty(lastname) &&this.isNotEmpty(sexo) && this.isNotEmpty(carrera) 
+&& this.isNotEmpty(fechaNaci)  ){
+  //todos los campos de perfil llenados
+  const user = this.auth.getAuth().currentUser;
+  if (user) {
+    this.firestore.create({
+      path: ["Users", user.uid],
+      data: {
+        publicName: name,
+        publicLastname: lastname,
+        publicCareer: carrera,
+        publicFN: fechaNaci,
+        publicSex: sexo,
+        userId: this.auth.getAuth().currentUser?.uid,
+        photoUrl: "https://devmiasx.com/uploads/67380645a9ac3_user.png",
+        privacidad: "publico"
+  
+      },
+      onComplete: (docId: string) => {
+        alert("Perfil creado");
+  
+  
+        InputNombre.value = "";
+         InputApellido.value ="";
+         InputCarrera.value="";
+         InputFN.value="";
+  
+      },
+      onFail: (err: any) => {
+        alert(err);
+      }
+    });
+  } else {
+    alert("No hay un usuario autenticado.");
+  }
+}else{
+  console.log("Completa todos los campos")
 }
 
-
+}
+isNotEmpty(text: string){
+  return text != null && text.length > 0;
 }
 }
